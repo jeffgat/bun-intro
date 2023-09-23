1d1243b5bed10d19a6864e78ff689eac1d6be9b8
jeffgat <jeff.gtbn@gmail.com>
2023-09-23 10:12:38 -0600

    quickstart intro

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import figlet from 'figlet';
+
+const server = Bun.serve({
+    fetch() {
+        const body = figlet.textSync('Bun!');
+        return new Response(body);
+    },
+    port: 3000,
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
